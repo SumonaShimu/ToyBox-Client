d36c65863cc38c226dd5f0b29772e89fbaefc1c5
Sumona Akter Shimu <sumona59shimu@gmail.com>
2023-05-20 23:35:37 +0600

    search system added

--- src/Pages/AllToys.jsx
+++ src/Pages/AllToys.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 
 const AllToys = () => {
-    const toys = useLoaderData();
+    const alltoys = useLoaderData();
+    const [toys,setToys]=useState(alltoys);
+
+    const handleSearch=(e)=>{
+        e.preventDefault();
+        const searchText=e.target.searchText.value;
+        fetch(`https://toybox-server.vercel.app/search/${searchText}`)
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+            setToys(data)})
+    }
+
     console.log(toys)
     return (
         <div className="overflow-x-auto m-5">
+            <form onSubmit={handleSearch} className="w-full flex justify-center gap-4 my-5">
+            <input type="text" name='searchText' placeholder="Toy Name or Category" className="input input-bordered" required/>
+            <input type="submit" className="btn btn-secondary text-white" value="Search" />
+            </form>
             <table className="table table-compact w-full text-center">
                 <thead>
                     <tr>
--- src/Pages/NavBar.jsx
+++ src/Pages/NavBar.jsx
@@ -17,8 +17,8 @@ const NavBar = () => {
   );
 
   return (
-    <div className="navbar bg-base-100">
-      <div className="navbar-start">
+    <div className="navbar bg-base-200">
+      <div className="navbar-start max-h-20">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -28,10 +28,10 @@ const NavBar = () => {
           </ul>
         </div >
 
-        <h1 className='h-36 w-52 flex items-center'>
+        <div className='h-36 w-52 flex items-center'>
           <img src="https://raw.githubusercontent.com/SumonaShimu/ToyData/main/logoo.png" className="object-scale-down h-24 w-24" />
           <Link to="/" className="font-bold btn btn-ghost normal-case text-xl">T<span className='text-teal-600'>o</span>yB<span className='text-teal-600'>o</span>x</Link>
-        </h1>
+        </div>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
